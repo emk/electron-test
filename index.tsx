275ac2b74e6fd216ba50b4a7bfff502e0caa786c
Eric Kidd <git@randomhacks.net>
2016-12-17 11:33:13 -0500

    Set up Redux

--- index.tsx
+++ index.tsx
@@ -1,7 +1,62 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Action, createStore } from 'redux';
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('root')
-);
+enum CounterAction { Increment, Decrement }
+
+function counter(state = 0, action: Action): number {
+  switch (action.type) {
+    case CounterAction.Increment:
+      return state + 1
+    case CounterAction.Decrement:
+      return state - 1
+    default:
+      return state
+  }
+}
+
+interface ICounterProps {
+  value: number,
+  onIncrement: () => void,
+  onDecrement: () => void
+}
+
+interface INoState {
+}
+
+class Counter extends React.Component<ICounterProps, INoState> {
+  static propTypes = {
+    value: React.PropTypes.number.isRequired,
+    onIncrement: React.PropTypes.func.isRequired,
+    onDecrement: React.PropTypes.func.isRequired
+  }
+
+  render() {
+    const { value, onIncrement, onDecrement } = this.props
+    return (
+      <div>
+        <p>Counter: {value}</p>
+        <p>
+          <button onClick={onIncrement}>+</button>
+          <button onClick={onDecrement}>-</button>
+        </p>
+      </div>
+    )
+  }
+}
+
+const store = createStore(counter, 0)
+
+function render() {
+  ReactDOM.render(
+    <Counter
+      value={store.getState() || 0}
+      onIncrement={() => store.dispatch({ type: CounterAction.Increment })}
+      onDecrement={() => store.dispatch({ type: CounterAction.Decrement })}
+      />,
+    document.getElementById('root')
+  )
+}
+
+render()
+store.subscribe(render)
