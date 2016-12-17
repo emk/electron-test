d31da150142f38f6e10b44dd97fc8696fd04bc97
Eric Kidd <git@randomhacks.net>
2016-12-17 15:08:02 -0500

    Use `object-assign` to update state

--- index.tsx
+++ index.tsx
@@ -1,8 +1,9 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import * as Electron from 'electron'
-import { format as urlFormat } from 'url';
+import React = require('react')
+import ReactDOM = require('react-dom')
+import { createStore } from 'redux'
+import Electron = require('electron')
+import { format as urlFormat } from 'url'
+import objectAssign = require("object-assign")
 
 // Get access to dialog boxes in our main UI process.
 const dialog = Electron.remote.dialog
@@ -10,6 +11,16 @@ const dialog = Electron.remote.dialog
 // The state of our movie player.
 interface PlayerState {
   url?: string
+  width: number,
+  height: number
+}
+
+function newPlayerState(): PlayerState {
+  return {
+    url: undefined,
+    width: window.innerWidth,
+    height: window.innerHeight
+  }
 }
 
 type SET_URL = "SET_URL";
@@ -24,16 +35,31 @@ function setUrlAction(url: string): SetUrlAction {
   return { type: SET_URL, url: url }
 }
 
+type SET_SIZE = "SET_SIZE";
+const SET_SIZE: SET_SIZE = "SET_SIZE";
+
+type SetSizeAction = {
+  type: SET_SIZE,
+  width: number,
+  height: number
+}
+
+function setSizeAction(width: number, height: number): SetSizeAction {
+  return { type: SET_SIZE, width: width, height: height }
+}
+
 type OtherAction = { type: '' }
 const OtherAction: OtherAction = { type: '' }
 
-type PlayerAction = SetUrlAction | OtherAction
+type PlayerAction = SetUrlAction | SetSizeAction | OtherAction
 
-function player(state: PlayerState = {}, action: PlayerAction): PlayerState {
+function player(state: PlayerState = newPlayerState(),
+  action: PlayerAction): PlayerState {
   switch (action.type) {
     case SET_URL:
-      // TODO: Use immutability-helper for updates.
-      return { url: action.url }
+      return objectAssign(state, { url: action.url })
+    case SET_SIZE:
+      return objectAssign(state, { width: action.width, height: action.height })
     default:
       return state
   }
@@ -94,7 +120,7 @@ const store = createStore(player)
 function render() {
   ReactDOM.render(
     <Player
-      url={(store.getState() || {}).url}
+      url={(store.getState() || newPlayerState()).url}
       onSetUrl={(url) => store.dispatch(setUrlAction(url))}
       />,
     document.getElementById('root')
